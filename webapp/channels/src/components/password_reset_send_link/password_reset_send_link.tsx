f3a7653b3d556e98fe769deacae23f9f400f14d8
aninitdev <vsmartstar.one@gmail.com>
2026-02-01 19:03:19 +0700

    login signin ui (#5)
    * login signin ui
    
    * remove mattermost
    
    * password reset
    
    * preparing workspace
    
    * change mattermost to oktel text

--- webapp/channels/src/components/password_reset_send_link/password_reset_send_link.tsx
+++ webapp/channels/src/components/password_reset_send_link/password_reset_send_link.tsx
@@ -1,173 +1,174 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2015-present Oktel, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {defineMessage, FormattedMessage, injectIntl} from 'react-intl';
-import type {IntlShape} from 'react-intl';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { defineMessage, FormattedMessage, useIntl } from 'react-intl';
+import { useHistory, Link } from 'react-router-dom';
 
-import type {ActionResult} from 'mattermost-redux/types/actions';
-import {isEmail} from 'mattermost-redux/utils/helpers';
+import type { ActionResult } from 'mattermost-redux/types/actions';
+import { isEmail } from 'mattermost-redux/utils/helpers';
 
-import BackButton from 'components/common/back_button';
 import Input from 'components/widgets/inputs/input/input';
+import type { CustomizeHeaderType } from 'components/header_footer_route/header_footer_route';
+
+import gradientLoginPage from 'images/gradient-login-page.png';
+import 'components/login/login.scss';
 
 export interface Props {
-    intl: IntlShape;
     siteName?: string;
     actions: {
         sendPasswordResetEmail: (email: string) => Promise<ActionResult>;
     };
+    onCustomizeHeader?: CustomizeHeaderType;
 }
 
-interface State {
-    error: React.ReactNode;
-    updateText: React.ReactNode;
-}
+const PasswordResetSendLink = ({ siteName, actions, onCustomizeHeader }: Props) => {
+    const intl = useIntl();
+    const history = useHistory();
+    const [error, setError] = useState<React.ReactNode>(null);
+    const [updateText, setUpdateText] = useState<React.ReactNode>(null);
 
-export class PasswordResetSendLink extends React.PureComponent<Props, State> {
-    state = {
-        error: null,
-        updateText: null,
-    };
-    resetForm = React.createRef<HTMLFormElement>();
-    emailInput = React.createRef<HTMLInputElement>();
+    const emailInput = useRef<HTMLInputElement>(null);
 
-    componentDidMount() {
-        const {intl, siteName} = this.props;
+    useEffect(() => {
         document.title = intl.formatMessage(
             {
                 id: 'password_form.pageTitle',
                 defaultMessage: 'Password Reset | {siteName}',
             },
-            {siteName: siteName || 'Mattermost'},
+            { siteName: siteName || 'Oktel' },
         );
-    }
 
-    handleSendLink = async (e: React.FormEvent) => {
+    }, [intl, siteName]);
+
+    const handleSendLink = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        const email = this.emailInput.current!.value.trim().toLowerCase();
+        const email = emailInput.current?.value.trim().toLowerCase();
         if (!email || !isEmail(email)) {
-            this.setState({
-                error: (
-                    <FormattedMessage
-                        id='password_send.error'
-                        defaultMessage='Please enter a valid email address.'
-                    />
-                ),
-            });
+            setError(
+                <FormattedMessage
+                    id='password_send.error'
+                    defaultMessage='Please enter a valid email address.'
+                />
+            );
             return;
         }
 
-        // End of error checking clear error
-        this.setState({error: null});
+        setError(null);
 
-        const {data, error} = await this.props.actions.sendPasswordResetEmail(email);
+        const { data, error: sendError } = await actions.sendPasswordResetEmail(email);
         if (data) {
-            this.setState({
-                error: null,
-                updateText: (
-                    <div
-                        id='passwordResetEmailSent'
-                        className='reset-form alert alert-success'
-                    >
-                        <FormattedMessage
-                            id='password_send.link'
-                            defaultMessage='If the account exists, a password reset email will be sent to:'
-                        />
-                        <div>
-                            <b>{email}</b>
-                        </div>
-                        <br/>
-                        <FormattedMessage
-                            id='password_send.checkInbox'
-                            defaultMessage='Please check your inbox.'
-                        />
+            setError(null);
+            setUpdateText(
+                <div
+                    id='passwordResetEmailSent'
+                    className='reset-form alert alert-success'
+                >
+                    <FormattedMessage
+                        id='password_send.link'
+                        defaultMessage='If the account exists, a password reset email will be sent to:'
+                    />
+                    <div>
+                        <b>{email}</b>
                     </div>
-                ),
-            });
-            if (this.resetForm.current) {
-                this.resetForm.current.hidden = true;
-            }
-        } else if (error) {
-            this.setState({
-                error: error.message,
-                updateText: null,
-            });
+                    <br />
+                    <FormattedMessage
+                        id='password_send.checkInbox'
+                        defaultMessage='Please check your inbox.'
+                    />
+                </div>
+            );
+        } else if (sendError) {
+            setError(sendError.message);
+            setUpdateText(null);
         }
     };
 
-    render() {
-        let error = null;
-        if (this.state.error) {
-            error = (
-                <div className='form-group has-error'>
-                    <label className='control-label'>{this.state.error}</label>
-                </div>
-            );
-        }
-
-        let formClass = 'form-group';
-        if (error) {
-            formClass += ' has-error';
-        }
-
-        return (
-            <div>
-                <BackButton/>
-                <div className='col-sm-12'>
-                    <div className='signup-team__container'>
+    return (
+        <div className='login-body'>
+            <div className='login-body-content'>
+                <div className='login-body-message'>
+                    <h1 className='login-body-message-title'>{siteName || 'Oktel'}</h1>
+                    <div className='login-body-message-subtitle'>
                         <FormattedMessage
-                            id='password_send.title'
-                            tagName='h1'
-                            defaultMessage='Password Reset'
+                            id='web.root.signup_info'
+                            defaultMessage='All your team communication in one place, instantly searchable and accessible anywhere.'
                         />
-                        {this.state.updateText}
-                        <form
-                            onSubmit={this.handleSendLink}
-                            ref={this.resetForm}
-                        >
-                            <p>
-                                <FormattedMessage
-                                    id='password_send.description'
-                                    defaultMessage='To reset your password, enter the email address you used to sign up'
-                                />
-                            </p>
-                            <div className={formClass}>
-                                <Input
-                                    id='passwordResetEmailInput'
-                                    type='email'
-                                    name='email'
-                                    label={defineMessage({
-                                        id: 'password_send.email',
-                                        defaultMessage: 'Email',
-                                    })}
-                                    placeholder={defineMessage({
-                                        id: 'password_send.email.placeholder',
-                                        defaultMessage: 'Enter the email address you used to sign up',
-                                    })}
-                                    ref={this.emailInput}
-                                    spellCheck='false'
-                                    autoFocus={true}
-                                />
-                            </div>
-                            {error}
-                            <button
-                                id='passwordResetButton'
-                                type='submit'
-                                className='btn btn-primary'
-                            >
-                                <FormattedMessage
-                                    id='password_send.reset'
-                                    defaultMessage='Reset my password'
-                                />
-                            </button>
-                        </form>
+                    </div>
+                    <img
+                        className='auth-page-gradient'
+                        src={gradientLoginPage}
+                        alt='gradient'
+                    />
+                </div>
+                <div className='login-body-card'>
+                    <div className='login-body-card-content'>
+                        <div className='login-body-card-title'>
+                            <FormattedMessage
+                                id='password_send.title'
+                                defaultMessage='Password Reset'
+                            />
+                        </div>
+                        <div className='login-body-card-form'>
+                            {updateText || (
+                                <form onSubmit={handleSendLink}>
+                                    <p>
+                                        <FormattedMessage
+                                            id='password_send.description'
+                                            defaultMessage='To reset your password, enter the email address you used to sign up'
+                                        />
+                                    </p>
+                                    <Input
+                                        id='passwordResetEmailInput'
+                                        type='email'
+                                        name='email'
+                                        className='login-body-card-form-input'
+                                        label={defineMessage({
+                                            id: 'password_send.email',
+                                            defaultMessage: 'Email',
+                                        })}
+                                        placeholder={defineMessage({
+                                            id: 'password_send.email.placeholder',
+                                            defaultMessage: 'Enter the email address you used to sign up',
+                                        })}
+                                        ref={emailInput}
+                                        spellCheck='false'
+                                        autoFocus={true}
+                                        hasError={Boolean(error)}
+                                        containerClassName={error ? 'has-error' : ''}
+                                    />
+                                    {error && (
+                                        <div className='form-group has-error'>
+                                            <label className='control-label'>{error}</label>
+                                        </div>
+                                    )}
+                                    <button
+                                        id='passwordResetButton'
+                                        type='submit'
+                                        className='login-body-card-form-button-submit'
+                                    >
+                                        <FormattedMessage
+                                            id='password_send.reset'
+                                            defaultMessage='Reset my password'
+                                        />
+                                    </button>
+                                    <div className='login-body-card-form-link'>
+                                        <Link to='/login'>
+                                            <FormattedMessage
+                                                id='password_form.back'
+                                                defaultMessage='Back to Login'
+                                            />
+                                        </Link>
+                                    </div>
+                                </form>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
-export default injectIntl(PasswordResetSendLink);
+export default PasswordResetSendLink;
